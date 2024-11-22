295d00a06f6e40bdd7132a2311c2a35b582cbb05
Sullivan Lacheteau <sullivan.lacheteau@nventive.com>
2024-11-22 11:00:21 -0500

    feat(311699): Add Weather CRUD endpoints

--- template/src/Placeholder.Core/Weather/IWeatherRepository.cs
+++ template/src/Placeholder.Core/Weather/IWeatherRepository.cs
@@ -4,5 +4,9 @@ namespace Placeholder.Core;
 
 public interface IWeatherRepository
 {
-    Task<Weather[]> GetWeather();
+    Task<IEnumerable<Weather>> GetWeathers();
+    Task<Weather?> GetWeather(DateTime date);
+    Task<Weather?> CreateWeather(Weather weather);
+    Task<Weather?> UpdateWeather(Weather weather);
+    Task DeleteWeather(DateTime date);
 }
