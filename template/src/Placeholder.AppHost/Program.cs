b20fe9f469f9cb15330f69d0303416fccbec0fd6
Sullivan Lacheteau <sullivan.lacheteau@nventive.com>
2024-12-16 16:27:13 -0500

    feat: configure App Insights for Aspire telemetry

--- template/src/Placeholder.AppHost/Program.cs
+++ template/src/Placeholder.AppHost/Program.cs
@@ -4,15 +4,20 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var weatherDb = builder.ConfigurePostgresDatabase("weather", builder.AddParameter("postgresUsername"), builder.AddParameter("postgresPassword", secret: true));
 
+var appInsights = builder.AddConnectionString("AppInsights", "APPLICATIONINSIGHTS_CONNECTION_STRING");
+
 var apiService = builder.AddProject<Projects.Placeholder_ApiService>("apiservice")
     .WithExternalHttpEndpoints()
-    .WithReference(weatherDb);
+    .WithReference(weatherDb)
+    .WithReference(appInsights);
 
 builder.AddProject<Projects.Placeholder_Migration>("migration")
-    .WithReference(weatherDb);
+    .WithReference(weatherDb)
+    .WithReference(appInsights);
 
 builder.AddProject<Projects.Placeholder_Web>("webfrontend")
     .WithExternalHttpEndpoints()
-    .WithReference(apiService);
+    .WithReference(apiService)
+    .WithReference(appInsights);
 
 builder.Build().Run();
