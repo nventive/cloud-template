c839558c994f8129ada2625b6ff440364101289b
Sullivan Lacheteau <sullivan.lacheteau@nventive.com>
2024-12-17 16:56:29 -0500

    feat: automatic provisioning

--- template/src/Placeholder.AppHost/Program.cs
+++ template/src/Placeholder.AppHost/Program.cs
@@ -4,7 +4,10 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var weatherDb = builder.ConfigurePostgresDatabase("weather", builder.AddParameter("postgresUsername"), builder.AddParameter("postgresPassword", secret: true));
 
-var appInsights = builder.AddConnectionString("AppInsights", "APPLICATIONINSIGHTS_CONNECTION_STRING");
+// Use the following for automatic provisioning:
+var appInsights = builder.AddAzureApplicationInsights("appinsights");
+// Use the following for manual provisioning and set a user secret for "ConnectionStrings:AppInsights"
+//var appInsights = builder.AddConnectionString("AppInsights", "APPLICATIONINSIGHTS_CONNECTION_STRING");
 
 var apiService = builder.AddProject<Projects.Placeholder_ApiService>("apiservice")
     .WithExternalHttpEndpoints()
