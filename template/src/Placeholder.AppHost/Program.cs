ae6dae2481af6ff8eabf38026c6beaf6c7f9d249
Mounir Abdous <mounir.abdous@nventive.com>
2024-12-18 14:41:37 -0500

    feat: add parameter for appinsights

--- template/src/Placeholder.AppHost/Program.cs
+++ template/src/Placeholder.AppHost/Program.cs
@@ -4,23 +4,25 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var weatherDb = builder.ConfigurePostgresDatabase("weather", builder.AddParameter("postgresUsername"), builder.AddParameter("postgresPassword", secret: true));
 
+var apiService = builder.AddProject<Projects.Placeholder_ApiService>("apiservice")
+    .WithExternalHttpEndpoints()
+    .WithReference(weatherDb);
+
+var migration = builder.AddProject<Projects.Placeholder_Migration>("migration")
+    .WithReference(weatherDb);
+
+var webfrontend = builder.AddProject<Projects.Placeholder_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService);
+
+#if appInsights
 // Use the following for automatic provisioning:
 var appInsights = builder.AddAzureApplicationInsights("appinsights");
 // Use the following for manual provisioning and set a user secret for "ConnectionStrings:AppInsights"
 //var appInsights = builder.AddConnectionString("AppInsights", "APPLICATIONINSIGHTS_CONNECTION_STRING");
-
-var apiService = builder.AddProject<Projects.Placeholder_ApiService>("apiservice")
-    .WithExternalHttpEndpoints()
-    .WithReference(weatherDb)
-    .WithReference(appInsights);
-
-builder.AddProject<Projects.Placeholder_Migration>("migration")
-    .WithReference(weatherDb)
-    .WithReference(appInsights);
-
-builder.AddProject<Projects.Placeholder_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(apiService)
-    .WithReference(appInsights);
+apiService.WithReference(appInsights);
+migration.WithReference(appInsights);
+webfrontend.WithReference(appInsights);
+#endif
 
 builder.Build().Run();
