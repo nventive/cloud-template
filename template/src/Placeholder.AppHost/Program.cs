a85210afc4a4b4dff0f644d16f9c7638109d16dc
Alexandra-Maude Grenier <am.grenier@nventive.com>
2025-01-08 16:04:13 -0500

    feat: add blob storage + update libs

--- template/src/Placeholder.AppHost/Program.cs
+++ template/src/Placeholder.AppHost/Program.cs
@@ -4,9 +4,17 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var weatherDb = builder.ConfigurePostgresDatabase("weather", builder.AddParameter("postgresUsername"), builder.AddParameter("postgresPassword", secret: true));
 
+var storage = builder.AddAzureStorage("storage");
+if (builder.ExecutionContext.IsRunMode)
+{
+    storage.RunAsEmulator();
+}
+var blobs = storage.AddBlobs("blobs");
+
 var apiService = builder.AddProject<Projects.Placeholder_ApiService>("apiservice")
     .WithExternalHttpEndpoints()
-    .WithReference(weatherDb);
+    .WithReference(weatherDb)
+    .WithReference(blobs);
 
 builder.AddProject<Projects.Placeholder_Migration>("migration")
     .WithReference(weatherDb);
